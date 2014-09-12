8b8d6e4b8ea808f34f08dbf7e9b3acbac787466a
John DeSilva <desilvjo@umich.edu>
2014-09-12 13:28:42 -0400

    Organize

---
+++ frequency.rb
@@ -0,0 +1,31 @@
+module Cryptography
+  module Frequency
+
+    def ngram_frequency(n)
+      freq = {}
+      chars.each_cons(n).map do |a|
+        ngram = a.join
+        freq[ngram] ||= 0
+        freq[ngram] += 1
+      end
+      freq
+    end
+
+    def letter_frequency
+      ngram_frequency(1)
+    end
+
+    def digram_frequency
+      ngram_frequency(2)
+    end
+
+    def trigram_frequency
+      ngram_frequency(3)
+    end
+
+  end
+end
+
+class String
+  include Cryptography::Frequency
+end
